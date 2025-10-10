9301d51ec8bdf93fa5e3932d415160749c8182b0
bruno.silva <bruno.silva@users.noreply.quidgest.pt>
2025-10-10 14:46:09 +0100

    Build version: 115 | Genio version: 374.11 | Database version: 2529 | Platform: VUE

--- GEN_QUIDGEST/QUIDMOVIES/CSGenio.core/config/Configuration.cs
+++ GEN_QUIDGEST/QUIDMOVIES/CSGenio.core/config/Configuration.cs
@@ -113,7 +113,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Solution build version
         /// </summary>
-        public const int BuildVersionGen = 114;
+        public const int BuildVersionGen = 115;
 
         /// <summary>
         /// Solution release version
